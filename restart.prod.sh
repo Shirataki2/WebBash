b75fcb86dded2022698a65d8511bfe6e5d1818bf
Shirataki2 <ahahah114514@gmail.com>
2020-06-09 18:44:21 +0900

    [ADD] CI/CD config

--- restart.prod.sh
+++ restart.prod.sh
@@ -1,7 +1,5 @@
 docker-compose -f docker-compose.prod.yml down
-docker-compose -f docker-compose.dev.yml up -d frontend
-docker-compose -f docker-compose.dev.yml exec frontend yarn install
+docker-compose -f docker-compose.prod.yml run --entrypoint "bash -c 'yarn install && yarn build'" frontend
 docker-compose -f docker-compose.prod.yml build
 docker-compose -f docker-compose.prod.yml up -d es01 es02 mongo
-docker-compose -f docker-compose.prod.yml up frontend
 docker-compose -f docker-compose.prod.yml up -d --scale api=8 kibana api proxy
