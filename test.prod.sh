a0d3d2854ba3bd3270c2a03be1358d7d391f88cc
Shirataki2 <ahahah114514@gmail.com>
2020-06-09 23:30:25 +0900

    [FIX] Scale を1にした．

--- test.prod.sh
+++ test.prod.sh
@@ -3,8 +3,8 @@ docker-compose -f docker-compose.test.yml build
 docker-compose -f docker-compose.test.yml run --entrypoint "bash -c 'yarn install --production=false && yarn build'" frontend
 docker-compose -f docker-compose.test.yml up -d es01 es02 mongo
 sleep 20
-docker-compose -f docker-compose.test.yml up -d --scale api=8 kibana api proxy
+docker-compose -f docker-compose.test.yml up -d api proxy
 
-docker-compose -f docker-compose.test.yml exec api pytest
+docker-compose -f docker-compose.test.yml exec -T api pytest
 
 docker-compose -f docker-compose.test.yml down
