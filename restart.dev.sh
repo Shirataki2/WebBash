4418dc09c7338c40aa3a4c80c4a693ffc3d7f3a6
Shirataki2 <ahahah114514@gmail.com>
2020-06-10 14:05:02 +0900

    Test時にxmlを作成しcodecovに反映させるテスト

---
+++ restart.dev.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo '[*] Down Docker Container'
+docker-compose -f docker-compose.dev.yml down --remove-orphans
+echo '[*] Build Images'
+docker-compose -f docker-compose.dev.yml build
+echo '[*] Prepare Frontend'
+docker-compose -f docker-compose.dev.yml up -d frontend
+echo '[*] Run Database'
+docker-compose -f docker-compose.dev.yml up -d es01 mongo
+echo '[*] Wait for Start Database'
+sleep 20
+echo '[*] Run Services'
+docker-compose -f docker-compose.dev.yml up -d api proxy
+echo '[I] Service Started!'
+echo -e "[I] To Run Frontend, execute \033[0;34m'docker-compose -f docker-compose.dev.yml exec frontend yarn serve'\033[0;39m"
+echo "    and access to http://<host_ip>:<frontend_port>"
+
