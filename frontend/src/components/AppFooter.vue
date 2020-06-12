978a4d06f249b2314a1a6ae5e7450ab766cf91cb
Shirataki2 <ahahah114514@gmail.com>
2020-06-12 15:38:04 +0900

    Headerなどコード量が嵩んでいるところを別ファイルにした

---
+++ frontend/src/components/AppFooter.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-footer>
+    <v-col
+      class="text-center ma-n2"
+      style="font-size: 0.8em"
+      padless
+      cols="12"
+    >
+      Version 1.1.1 |
+      &copy; {{ new Date().getFullYear() }} — <strong>FF</strong> (Twitter: <a
+        target="_blank"
+        href="https://twitter.com/fujifog"
+      >@fujifog</a>) | <a
+        target="_blank"
+        href="/api/docs"
+      >API Documentation</a>
+    </v-col>
+  </v-footer>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+
+@Component
+class ThemeSwitch extends Vue {}
+export default ThemeSwitch;
+</script>
